22bb9c03cac3b5c6cbf1c9910c27b997e0eee610
trevorstam <trevorstam@gmail.com>
2019-06-15 14:42:06 -0700

    create basic lambda function

---
+++ index.js
@@ -0,0 +1,36 @@
+import Request from 'request-promise';
+
+//-----------------------------------------------------
+//--LAMBDA FUNCTION
+//-----------------------------------------------------
+
+const dispatcher = (event) => {
+  let response = {
+    sessionAttributes: event.sessionAttributes,
+    dialogAction: {
+      type: 'Close',
+      fulfillmentState: '',
+      message: {
+        'contentType': 'PlainText',
+        'content': ''
+      }
+    }
+  };
+  switch(event.currentIntent.name){
+    case 'AboutBotIntent':
+      response.dialogAction.fulfillmentState = 'Fulfilled';
+      response.dialogAction.message.content = 'Created by Trevor Stam';
+      break;
+    case 'LatLngIntent':
+      break;
+    default:
+      response.dialogAction.fulfillmentState = 'Failed';
+      response.dialogAction.message.content = 'Please rephrase';
+      break;
+  }
+  return response;
+}
+
+exports.handler = (event) => {
+  return dispatcher;
+}
